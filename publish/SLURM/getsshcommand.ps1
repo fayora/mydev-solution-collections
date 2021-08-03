ed827220db950b04abb74332a935aa359bf3770c
fayora <10859965+fayora@users.noreply.github.com>
2021-08-03 14:40:34 +1200

    Add files via upload

---
+++ publish/SLURM/getsshcommand.ps1
@@ -0,0 +1,44 @@
+[CmdletBinding()]
+[OutputType([psobject])]
+param (
+    [Parameter( Mandatory = $true, ValueFromPipeline = $true)]
+    [string]$subscriptionId,
+    [Parameter( Mandatory = $true, ValueFromPipeline = $true)]
+    [string]$resourceGroupName,
+    [Parameter( Mandatory = $true, ValueFromPipeline = $true)]
+    [string]$publicIpAddressName,
+    [Parameter( Mandatory = $true, ValueFromPipeline = $true)]
+    [string]$adminUserName
+)
+process {
+    try {
+
+        # Get an access token for managed identities for Azure resources
+        $response = Invoke-WebRequest `
+            -Uri 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fmanagement.azure.com%2F' `
+            -Headers @{Metadata = "true" }
+        $content = $response.Content | ConvertFrom-Json
+        $access_token = $content.access_token
+
+        
+        # Use the access token to get resource information for the VM
+        $publicIpAddressResponse = Invoke-WebRequest `
+            -Uri "https://management.azure.com/subscriptions/$subscriptionId/resourceGroups/$resourceGroupName/providers/Microsoft.Network/publicIPAddresses/$publicIpAddressName\?api-version=2020-11-01" `
+            -Method GET `
+            -ContentType "application/json" `
+            -Headers @{ Authorization = "Bearer $access_token" } 
+        
+        $publicIpAddressContent = $publicIpAddressResponse.Content | ConvertFrom-Json
+       
+        if (!$publicIpAddressContent.properties.ipAddress) { throw "IP address does not exist." }
+       
+        $ipAddress = $publicIpAddressContent.properties.ipAddress
+       
+        
+        New-Object -Property @{ReturnText = "$adminUserName@$ipAddress" } -TypeName psobject
+    }
+    catch {
+        Write-Host "Unable to get IP address." $_.Exception.Message
+    }
+}
+
---
+++ publish/SLURM/releasenotes.md
@@ -0,0 +1,5 @@
+# SLURM on CentOS 7.1 HPC 
+
+## [v2021.16.07.1](https://github.com/openservicebrokerapi/servicebroker/blob/v2.16/spec.md)
+2021.16.07
+* Add a SLURM cluster with head node and n worker node.
