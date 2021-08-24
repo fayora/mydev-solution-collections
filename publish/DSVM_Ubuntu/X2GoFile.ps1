6b83c3ce8869c4fb3418ce9a3cf19ed08e922e99
Felipe Ayora <10859965+fayora@users.noreply.github.com>
2021-08-24 15:56:19 +1200

    Added file action for X2Go

---
+++ publish/DSVM_Ubuntu/X2GoFile.ps1
@@ -0,0 +1,14 @@
+[CmdletBinding()]
+[OutputType([psobject])]
+param (
+    [Parameter( Mandatory = $true, ValueFromPipeline = $true)]
+    [string]$fullyQualifiedDomainName,
+    [Parameter( Mandatory = $true, ValueFromPipeline = $true)]
+    [string]$AdminUsername
+)
+process {
+    $X2GoInfo = "Open your X2Go Client application and connect to your virtual machine by using the following session information:`r`n- Host: $fullyQualifiedDomainName`r`n- Login: $AdminUsername`r`n-SSH port: 22`r`n- Session type: XFCE.`r`n`r`nIf you don't have X2Go Client already installed on your computer, you can go here: https://wiki.x2go.org/doku.php/doc:installation:x2goclient"
+    New-Object -Property @{ReturnText = "{""fileName"":""X2GoConnectionInstructions.txt"", ""fileContent"":""$X2GoInfo""}" } -TypeName psobject
+}
+
+
