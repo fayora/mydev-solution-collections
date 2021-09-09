9d72b46bb94151d3600a8bd2fc6c92ccc591b1d0
Felipe Ayora <10859965+fayora@users.noreply.github.com>
2021-09-09 20:30:42 +1200

    And again testing URL solutions

--- publish/Databricks_Workspace/connectToWorkspace.ps1
+++ publish/Databricks_Workspace/connectToWorkspace.ps1
@@ -2,11 +2,9 @@
 [OutputType([psobject])]
 param (
     [Parameter( Mandatory = $true, ValueFromPipeline = $true)]
-    [string]$workspaceURL,
-    [Parameter( Mandatory = $true, ValueFromPipeline = $true)]
-    [string]$workspaceURL2
+    [string]$workspaceURL
 )
 process {
-    $workspaceURL3 = "URL1: " + $workspaceURL + " ; URL2: " + $workspaceURL2
-    New-Object -Property @{ReturnText = "$workspaceURL3"} -TypeName psobject
+    $workspaceURLComplete = "https://" + $workspaceURL
+    New-Object -Property @{ReturnText = "$workspaceURLComplete"} -TypeName psobject
 }
