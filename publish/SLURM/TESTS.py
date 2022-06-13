8e3794e8bf09db72c42733eae08928734611e5af
Felipe Ayora <10859965+fayora@users.noreply.github.com>
2022-06-13 12:26:35 +1200

    Created

---
+++ publish/SLURM/TESTS.py
@@ -0,0 +1,85 @@
+from urllib.request import urlopen, Request
+from time import sleep
+import json
+
+# NEW!!!
+from functools import wraps
+from urllib.error import URLError
+
+
+# def retry(howmany):
+#     def tryIt(func):
+#         def f():
+#             attempts = 0
+#             while attempts < howmany:
+#                 try:
+#                     return func()
+#                 except:
+#                     attempts += 1
+#         return f
+#     return tryIt
+
+# @retry(5)
+
+def retry(ExceptionToCheck, tries=4, delay=3, backoff=2, logger=None):
+    """Retry calling the decorated function using an exponential backoff.
+
+    http://www.saltycrane.com/blog/2009/11/trying-out-retry-decorator-python/
+    original from: http://wiki.python.org/moin/PythonDecoratorLibrary#Retry
+
+    :param ExceptionToCheck: the exception to check. may be a tuple of
+        exceptions to check
+    :type ExceptionToCheck: Exception or tuple
+    :param tries: number of times to try (not retry) before giving up
+    :type tries: int
+    :param delay: initial delay between retries in seconds
+    :type delay: int
+    :param backoff: backoff multiplier e.g. value of 2 will double the delay
+        each retry
+    :type backoff: int
+    :param logger: logger to use. If None, print
+    :type logger: logging.Logger instance
+    """
+    def deco_retry(f):
+
+        @wraps(f)
+        def f_retry(*args, **kwargs):
+            mtries, mdelay = tries, delay
+            while mtries > 1:
+                try:
+                    return f(*args, **kwargs)
+                except ExceptionToCheck as e:
+                    message = "%s, Retrying in %d seconds..." % (str(e), mdelay)
+                    if logger:
+                        logger.warning(msg)
+                    else:
+                        print (message)
+                    sleep(mdelay)
+                    mtries -= 1
+                    mdelay *= backoff
+            return f(*args, **kwargs)
+        return f_retry  # true decorator
+    return deco_retry
+
+@retry(URLError, tries=4, delay=5, backoff=2)
+def get_vm_metadata():
+    metadata_url = "http://169.254.169.254/metadata/instance?api-version=2017-08-01"
+    metadata_req = Request(metadata_url, headers={"Metadata": True})
+    print("Getting VM Metadata...")
+    metadata_response = urlopen(metadata_req, timeout=2)
+    return json.load(metadata_response)
+
+@retry(URLError, tries=4, delay=5, backoff=2)
+def get_vm_managed_identity():
+    # Managed Identity may  not be available immediately at VM startup...
+    # Test/Pause/Retry to see if it gets assigned
+    metadata_url = 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https://management.azure.com/'
+    metadata_req = Request(metadata_url, headers={"Metadata": True})
+    print("Getting the Managed Identity of the VM...")
+    metadata_response = urlopen(metadata_req, timeout=2)
+    return json.load(metadata_response)
+
+result_metadata = get_vm_metadata()
+print('Metadata=', result_metadata)
+result_msi = get_vm_managed_identity()
+print('MSI=', result_msi)
