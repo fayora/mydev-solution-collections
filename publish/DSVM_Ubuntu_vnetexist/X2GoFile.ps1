6932e6e411ea6eac4366ea4d51f495c8d4b99379
miguel-abulencia <miguel.abulencia@bizdata.com.au>
2025-04-09 14:37:24 +0800

    action x2go

--- publish/DSVM_Ubuntu_vnetexist/X2GoFile.ps1
+++ publish/DSVM_Ubuntu_vnetexist/X2GoFile.ps1
@@ -1,13 +1,13 @@
 [CmdletBinding()]
 [OutputType([psobject])]
 param (
-    [Parameter( Mandatory = $true, ValueFromPipeline = $true)]
-    [string]$fullyQualifiedDomainName,
-    [Parameter( Mandatory = $true, ValueFromPipeline = $true)]
+    [Parameter(Mandatory = $true)]
+    [string]$privateIpAddress,
+    [Parameter(Mandatory = $true)]
     [string]$loginUsername
 )
 process {
-    $X2GoInfo = "Open your X2Go Client application and connect to your virtual machine by using the following session information:\n - Host: $fullyQualifiedDomainName\n - Login: $loginUsername\n - SSH port: 22\n - Session type: XFCE\n\nIf you don't have X2Go Client already installed on your computer, you can go here: https://wiki.x2go.org/doku.php/doc:installation:x2goclient \n\n*NOTE: if you get an error when trying to connect, wait a couple of minutes and try again. The virtual machine needs time to load and configure everything when it first starts, before it becomes fully available."
+    $X2GoInfo = "Open your X2Go Client application and connect to your virtual machine by using the following session information:\n - Host: $privateIpAddress\n - Login: $loginUsername\n - SSH port: 22\n - Session type: XFCE\n\nIf you don't have X2Go Client already installed on your computer, you can go here: https://wiki.x2go.org/doku.php/doc:installation:x2goclient \n\n*NOTE: if you get an error when trying to connect, wait a couple of minutes and try again. The virtual machine needs time to load and configure everything when it first starts, before it becomes fully available."
     New-Object -Property @{ReturnText = "{""fileName"":""X2GoConnectionInstructions.txt"", ""fileContent"":""$X2GoInfo""}" } -TypeName psobject
 }
 
