b1abe369700a49603bc4d77245446e8a7fd69d91
fayora <10859965+fayora@users.noreply.github.com>
2025-05-12 23:42:26 +1200

    Updated for new template

--- publish/DSVM_Windows_SecureVNet/turnVMOnOff.ps1
+++ publish/DSVM_Windows_SecureVNet/turnVMOnOff.ps1
@@ -1,78 +1,65 @@
-[CmdletBinding()]
-[OutputType([psobject])]
-param (
-    [Parameter( Mandatory = $true, ValueFromPipeline = $true)]
-    [string]$subscriptionId,
-    [Parameter( Mandatory = $true, ValueFromPipeline = $true)]
-    [string]$resourceGroupName1,
-    [Parameter( Mandatory = $true, ValueFromPipeline = $true)]
-    [string]$deployedVirtualMachineName
-)
+# This script stops or starts the VM based on its current status.
+try {
 
+    # Get an access token for managed identities for Azure resources
+    $response = Invoke-WebRequest `
+        -Uri 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fmanagement.azure.com%2F' `
+        -Headers @{Metadata = "true" }
+    $content = $response.Content | ConvertFrom-Json
+    $access_token = $content.access_token
 
-process {
-    try {
+    
+    # Use the access token to get resource information for the VM
+    $currentStatusResponse = Invoke-WebRequest `
+        -Uri "https://management.azure.com/subscriptions/$subscriptionId/resourceGroups/$resourceGroupName/providers/Microsoft.Compute/virtualMachines/$deployedVirtualMachineName/instanceView?api-version=2021-03-01" `
+        -Method GET `
+        -ContentType "application/json" `
+        -Headers @{ Authorization = "Bearer $access_token" } 
+    
+    $currentStatusContent = $currentStatusResponse.Content | ConvertFrom-Json
+    $vmStatus = $currentStatusContent.statuses[1].displayStatus
+    
+    if ($vmStatus -eq "VM running") {
+        # The VM is currently running, so stopping it
+        $statusChangeResponse = Invoke-WebRequest `
+        -Uri "https://management.azure.com/subscriptions/$subscriptionId/resourceGroups/$resourceGroupName/providers/Microsoft.Compute/virtualMachines/$deployedVirtualMachineName/deallocate?api-version=2021-03-01" `
+        -Method POST `
+        -ContentType "application/json" `
+        -Headers @{ Authorization = "Bearer $access_token" } 
 
-        # Get an access token for managed identities for Azure resources
-        $response = Invoke-WebRequest `
-            -Uri 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fmanagement.azure.com%2F' `
-            -Headers @{Metadata = "true" }
-        $content = $response.Content | ConvertFrom-Json
-        $access_token = $content.access_token
+        ###NEEDS CODE TO CHECK 200 vs. DIFF RETURN! <--LOOK AT LOOME API CODE
 
-        
-        # Use the access token to get resource information for the VM
-        $currentStatusResponse = Invoke-WebRequest `
-            -Uri "https://management.azure.com/subscriptions/$subscriptionId/resourceGroups/$resourceGroupName1/providers/Microsoft.Compute/virtualMachines/$deployedVirtualMachineName/instanceView?api-version=2021-03-01" `
-            -Method GET `
-            -ContentType "application/json" `
-            -Headers @{ Authorization = "Bearer $access_token" } 
-        
-        $currentStatusContent = $currentStatusResponse.Content | ConvertFrom-Json
-        $vmStatus = $currentStatusContent.statuses[1].displayStatus
-       
-        if ($vmStatus -eq "VM running") {
-            # The VM is currently running, so stopping it
+        $result = "The VM is currently stopping."
+
+    } elseif ($vmStatus -eq "VM deallocated") {
+            # The VM is currently stopped, so starting it
             $statusChangeResponse = Invoke-WebRequest `
-            -Uri "https://management.azure.com/subscriptions/$subscriptionId/resourceGroups/$resourceGroupName1/providers/Microsoft.Compute/virtualMachines/$deployedVirtualMachineName/deallocate?api-version=2021-03-01" `
+            -Uri "https://management.azure.com/subscriptions/$subscriptionId/resourceGroups/$resourceGroupName/providers/Microsoft.Compute/virtualMachines/$deployedVirtualMachineName/start?api-version=2021-03-01" `
             -Method POST `
             -ContentType "application/json" `
             -Headers @{ Authorization = "Bearer $access_token" } 
 
             ###NEEDS CODE TO CHECK 200 vs. DIFF RETURN! <--LOOK AT LOOME API CODE
 
-            $result = "The VM is currently stopping."
-
-        } elseif ($vmStatus -eq "VM deallocated") {
-             # The VM is currently stopped, so starting it
-             $statusChangeResponse = Invoke-WebRequest `
-             -Uri "https://management.azure.com/subscriptions/$subscriptionId/resourceGroups/$resourceGroupName1/providers/Microsoft.Compute/virtualMachines/$deployedVirtualMachineName/start?api-version=2021-03-01" `
-             -Method POST `
-             -ContentType "application/json" `
-             -Headers @{ Authorization = "Bearer $access_token" } 
-
-             ###NEEDS CODE TO CHECK 200 vs. DIFF RETURN! <--LOOK AT LOOME API CODE
-
-             $result = "The VM is currently starting."
-             
-        } elseif ($vmStatus -eq "VM stopped") {
-            # The VM was incorrectly shut down by the user from the operating system, so stopping it and showing a message
-            $statusChangeResponse = Invoke-WebRequest `
-            -Uri "https://management.azure.com/subscriptions/$subscriptionId/resourceGroups/$resourceGroupName1/providers/Microsoft.Compute/virtualMachines/$deployedVirtualMachineName/deallocate?api-version=2021-03-01" `
-            -Method POST `
-            -ContentType "application/json" `
-            -Headers @{ Authorization = "Bearer $access_token" }
-            
-            $result = "$deployedVirtualMachineName was incorrectly shut down from the operating system, which continues to accrue charges! Deallocating $deployedVirtualMachineName first. Please wait a couple of minutes and try starting it again."
-
-        } else {
-            $result = "The VM is currently in a transitioning state: $vmStatus. Wait a couple of minutes and try again."
-        }
+            $result = "The VM is currently starting."
+    } elseif ($vmStatus -eq "VM stopped") {
+        # The VM was incorrectly shut down by the user from the operating system, so stopping it and showing a message
+        $statusChangeResponse = Invoke-WebRequest `
+        -Uri "https://management.azure.com/subscriptions/$subscriptionId/resourceGroups/$resourceGroupName/providers/Microsoft.Compute/virtualMachines/$deployedVirtualMachineName/deallocate?api-version=2021-03-01" `
+        -Method POST `
+        -ContentType "application/json" `
+        -Headers @{ Authorization = "Bearer $access_token" }
         
-        New-Object -Property @{ReturnText = "$result" } -TypeName psobject
+        $result = "$deployedVirtualMachineName was incorrectly shut down from the operating system, which continues to accrue charges! Deallocating $deployedVirtualMachineName first. Please wait a couple of minutes and try starting it again."
+
+    } else {
+    $result = "The VM is currently in a transitioning state: $vmStatus. Wait a couple of minutes and try again."
     }
-    catch {
-        Write-Error "Unable to determine the status of this VM. Please try again in a few minutes." $_.Exception.Message
-        New-Object -Property @{ReturnText = "Unable to determine the status of this VM. Please try again in a few minutes. " + $_.Exception.Message } -TypeName psobject
-    }
+    
+    New-Object -Property @{ReturnText = "$result" } -TypeName psobject
 }
+catch {
+    Write-Error "Unable to determine the status of this VM. Please try again in a few minutes." $_.Exception.Message
+    New-Object -Property @{ReturnText = "Unable to determine the status of this VM. Please try again in a few minutes. " + $_.Exception.Message } -TypeName psobject
+}
+
