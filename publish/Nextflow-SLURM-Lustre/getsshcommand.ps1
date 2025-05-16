41d78c50f2630e91839f8d88c1e7f48a94782e77
fayora <10859965+fayora@users.noreply.github.com>
2025-05-16 22:19:19 +1200

    Add file name to .pem result

--- publish/Nextflow-SLURM-Lustre/getsshcommand.ps1
+++ publish/Nextflow-SLURM-Lustre/getsshcommand.ps1
@@ -1,5 +1,13 @@
 $keepFile = 'True'
 
+# Get the trimmed name of the Solution Collection from the Resource Group name
+$stringArray = $resourceGroupName.Split("-")
+$solutionCollectionIndex = $stringArray.Count - 2
+$solutionCollectionName = $stringArray[$solutionCollectionIndex].Replace("_", "").Replace(".", "")
+
+# Specify the name of the private key file by using the name of the Solution Collection, so that it is easy to identify
+$downloadFileName = $solutionCollectionName + "-cluster.pem"
+
 try {
     # Only install modules that are used to avoid agent timeout (3mins)
     if (-Not (Get-Module -ListAvailable -Name Az.Accounts)) {
@@ -30,7 +38,7 @@ try {
     Disconnect-AzAccount
 
     # Assign value  to $result so it can be return to the UI
-    $result = "ssh hpcadmin@" + $schedulerPublicIpAddress + " -i <private key>"
+    $result = "ssh hpcadmin@" + $schedulerPublicIpAddress + " -i $downloadFileName"
 }
 catch {
     Write-Host "Unable to get ssh detail of the scheduler node." $_.Exception.Message
